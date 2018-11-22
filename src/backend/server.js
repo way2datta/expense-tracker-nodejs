259adf856015d05b969f6e8a6f26d0a9b39ad52f
Dagttatray Kale <way2datta@gmail.com>
2018-11-22 11:20:01 +0530

    Access forbidden

--- src/backend/server.js
+++ src/backend/server.js
@@ -6,6 +6,11 @@ const app = express();
 const port = 3000;
 
 app.use('/', routes);
+app.all('/server.*', function (req,res, next) {
+    res.status(403).send({
+       message: 'Access Forbidden'
+    });
+ });
 app.use(express.static('dist'));
 app.get('/admin', function(request, response) {
     response.sendFile(join(__dirname, './../frontend/index.html'));
