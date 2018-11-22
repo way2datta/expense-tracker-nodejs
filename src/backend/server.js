ee6ebfbb5b245ce8e04a221fa55dca698fb69bc6
Dagttatray Kale <way2datta@gmail.com>
2018-11-22 10:24:02 +0530

    Reorganized the server folder and added frontend index.html

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 import routes from './routes/rest-api';
+import { join } from 'path';
 
 const app = express();
 const port = 3000;
 
 app.use('/', routes);
+
+app.get('/admin', function(request, response) {
+    response.sendFile(join(__dirname, './../frontend/index.html'));
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
