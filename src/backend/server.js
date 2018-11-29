0cedc3d9964d13197a91baf02469730c3fc04126
Dagttatray Kale <way2datta@gmail.com>
2018-11-29 15:58:12 +0530

    Refactored server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,29 +1,56 @@
 import express from 'express';
-import { join } from 'path';
-import mongoose from 'mongoose';
-import servicesRoutes from './routes/services';
-import { databaseConnectionString, apiServerPort } from "./config";
 
 const app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.set('debug', true);
-mongoose.connect(databaseConnectionString);
+setupDatabase();
+setupBodyParser();
+setupApiRoutes();
+setupRestrictionForAccessingServerResources();
+setupMiddlewareThatServesStaticFiles();
+setupDefaultPage();
+setupApiServerPortAndListen();
 
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/api/', servicesRoutes);
-app.all('/server.*', (request, response) => {
-    response.status(403).send({
-        message: 'Access Forbidden',
+function setupDefaultPage() {
+    const join = require('path');
+    app.get('/', (request, response) => {
+        response.sendFile(join(__dirname, './../frontend/index.html'));
     });
-});
-app.use(express.static('dist'));
-app.get('/admin', (request, response) => {
-    response.sendFile(join(__dirname, './../frontend/index.html'));
-});
+}
 
-app.listen(apiServerPort, () => console.log(`Expense tracker app listening on port ${apiServerPort}!`));
+function setupApiServerPortAndListen() {
+    const apiServerPort = require("./config").apiServerPort;
+    app.listen(apiServerPort, () => console.log(`Expense tracker app listening on port ${apiServerPort}!`));
+}
+
+function setupMiddlewareThatServesStaticFiles() {
+    app.use(express.static('dist'));
+}
+
+function setupRestrictionForAccessingServerResources() {
+    app.all('/server.*', (request, response) => {
+        response.status(403).send({
+            message: 'Access Forbidden',
+        });
+    });
+}
+
+function setupBodyParser() {
+    const bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+}
+
+function setupDatabase() {
+    const databaseConnectionString = require("./config").databaseConnectionString;
+    const mongoose = require('mongoose');
+    mongoose.Promise = global.Promise;
+    mongoose.set('debug', true);
+    mongoose.connect(databaseConnectionString);
+}
+
+function setupApiRoutes() {
+    const servicesRoutes = require('./routes/services');
+    app.use('/api/', servicesRoutes);
+}
+
