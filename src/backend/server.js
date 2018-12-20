593a6b767b0b8fe1b372ca22ed2b44e3c417915a
Dagttatray Kale <way2datta@gmail.com>
2018-12-20 13:53:22 +0530

    .

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,52 +1,34 @@
 import express from 'express';
 import servicesRoutes from './routes/services';
 import DatabaseInitializer from "./database/DatabaseInitializer";
+
 const errorHandler = require('./ErrorHandler');
 
 const app = express();
 
 (new DatabaseInitializer()).initialize();
-setupBodyParser();
-setupApiRoutes();
-setupRestrictionForAccessingServerResources();
-setupMiddlewareThatServesStaticFiles();
-setupDefaultPage();
-setupApiServerPortAndListen();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/', servicesRoutes);
+
+app.all('/server.*', (request, response) => {
+    response.status(403).send({
+        message: 'Access Forbidden',
+    });
+});
+app.use(express.static('dist'));
+
+const AnyUrl = '*';
+const path = require('path');
+
+app.get(AnyUrl, (request, response) => {
+    response.sendFile("index.html", { root: path.join('dist') })
+});
+
 app.use(errorHandler);
 
-function setupDefaultPage() {
-    const AnyUrl = '*';
-    const join = require('path').join;
-    app.get(AnyUrl, (request, response) => {
-        // response.sendFile(join(__dirname, './index.html'));
-         response.sendFile('./index.html');
-    });
-}
-
-function setupApiServerPortAndListen() {
-    const apiServerPort = require("./config").apiServerPort;
-    app.listen(apiServerPort, () => console.log(`Expense tracker app listening on port ${apiServerPort}!`));
-}
-
-function setupMiddlewareThatServesStaticFiles() {
-    app.use(express.static('dist'));
-}
-
-function setupRestrictionForAccessingServerResources() {
-    app.all('/server.*', (request, response) => {
-        response.status(403).send({
-            message: 'Access Forbidden',
-        });
-    });
-}
-
-function setupBodyParser() {
-    const bodyParser = require('body-parser');
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-}
-
-function setupApiRoutes() {
-    app.use('/api/', servicesRoutes);
-}
-
+const apiServerPort = require("./config").apiServerPort;
+app.listen(apiServerPort, () => console.log(`Expense tracker app listening on port ${apiServerPort}!`));
