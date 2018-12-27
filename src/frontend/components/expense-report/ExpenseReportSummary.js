3182af8cc338d11eea8b9b3dafd86bdcc22b8136
Dagttatray Kale <way2datta@gmail.com>
2018-12-27 17:07:01 +0530

    Charts formatting and look and feel

--- src/frontend/components/expense-report/ExpenseReportSummary.js
+++ src/frontend/components/expense-report/ExpenseReportSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BarChart, Bar, Cell, CartesianGrid, XAxis, YAxis, Tooltip, LabelList } from 'recharts';
+import { BarChart, Bar, Cell, CartesianGrid, XAxis, YAxis, Tooltip, LabelList,ResponsiveContainer }
+    from 'recharts';
 import ExpenseReportModel from "./ExpenseReportModel";
 
 export default class ExpenseReportSummary extends React.Component {
@@ -17,32 +18,37 @@ export default class ExpenseReportSummary extends React.Component {
         });
     }
 
+    kFormatter(money) {
+        return money > 999 ? (money/1000).toFixed(1) + 'k' : money
+    }
+
     render() {
         return (
             <div>
                 <h3 className="heading">Expense summary reports</h3>
-
-                <BarChart width={900} height={400} data={this.state.datasource} >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="categoryName" height={50}
-                        tick={{stroke: 'red', strokeWidth: 1}}
-                        label={{ value: "Category", position: "insideBottom", offset: 0 }} 
-                    />
-                    <YAxis yAxisId="totalAmount" width={70}
-                        tick={{stroke: 'red', strokeWidth: 1}} 
-                        label={{ value: 'Total amount', angle: -90, position: 'insideLeft', textAnchor: 'middle' }}
-                    />
-                    <Tooltip />
-                    <CartesianGrid vertical={false} />
-                    <Bar yAxisId="totalAmount" dataKey="totalAmount">
-                        <LabelList fill="#2cc1e6" angle={-45} />
-                        {
-                            this.state.datasource.map((entry, index) => (
-                                <Cell key={`cell-${index}`} fill={index % 20} />
-                            ))
-                        }
-                    </Bar>
-                </BarChart>
+                <ResponsiveContainer width={900} height="50%">
+                    <BarChart data={this.state.datasource} height="90%" >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="categoryName" height={50}
+                            label={{ stroke: 'red', strokeWidth: 1, value: "Category", 
+                                position: "insideBottom", offset: 0 }} 
+                        />
+                        <YAxis yAxisId="totalAmount" width={70}
+                            label={{ stroke: 'red', strokeWidth: 1, value: 'Total amount in dollars.', 
+                                angle: -90, position: 'insideLeft', textAnchor: 'middle' }}
+                            tickFormatter={(value) => this.kFormatter(value)} 
+                        />
+                        <Tooltip formatter={(value) => new Intl.NumberFormat('en').format(value)} />
+                        <Bar yAxisId="totalAmount" dataKey="totalAmount">
+                            <LabelList fill="#2cc1e6" angle={-90} formatter={(value) => new Intl.NumberFormat('en').format(value)} />
+                            {
+                                this.state.datasource.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={index % 20} />
+                                ))
+                            }
+                        </Bar>
+                    </BarChart>
+                </ResponsiveContainer>
             </div>
         );
     }
