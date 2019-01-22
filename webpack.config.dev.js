ca46d61fe508ace4ed4feacf21257e85e236c4b7
Dagttatray Kale <way2datta@gmail.com>
2019-01-22 14:11:00 +0530

    Added required plugins

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,25 +8,56 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    mode: 'development',
+    target: 'web',
     module: {
         rules: [
             {
+                enforce: "pre",
                 test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+                options: {
+                    emitWarning: true,
+                    failOnError: false,
+                    failOnWarning: false
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['@babel/env', '@babel/preset-react'],
                 },
             },
             {
+                // Loads the javacript into html template provided.
+                // Entry point is set below in HtmlWebPackPlugin in Plugins 
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        //options: { minimize: true }
+                    }
+                ]
+            },
+            {
                 test:/\.(s*)css$/,
                 use:['style-loader','css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
             }
-        ],
+        ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/frontend/index.html',
-            filename: './index.html'
-        })
+            filename: './index.html',
+            excludeChunks: ['server']
+        }),
+
     ]
 };
