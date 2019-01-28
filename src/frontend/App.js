f3155b4f68a0bfcaa7e37e3f3e3216f02ed46293
Dagttatray Kale <way2datta@gmail.com>
2019-01-28 16:54:31 +0530

    Renaming router

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/_Navbar';
-import RouterContent from './components/Router';
+import RouterContent from './components/_Router';
 import { Footer } from './components/_Footer';
 import './styles/app.scss';
 
