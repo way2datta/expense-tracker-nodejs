9120398e885a85dab82202792eb1f4816c67942c
Dagttatray Kale <way2datta@gmail.com>
2019-02-04 15:59:05 +0530

    Added public path

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,6 +7,7 @@ module.exports = {
     entry: './src/frontend/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
         filename: 'bundle.js',
     },
     mode: 'development',
